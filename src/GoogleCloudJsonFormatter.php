e2495aa502cc0086aee178d29a261dacab2ff0f5
Rob Vella <me@robvella.com>
2016-12-15 15:42:18 -0800

    Return blank

--- src/GoogleCloudJsonFormatter.php
+++ src/GoogleCloudJsonFormatter.php
@@ -67,7 +67,7 @@ class GoogleCloudJsonFormatter extends JsonFormatter
 
         // Drop un-necessary messages
         if (!empty($formatted['route_parameters']) || preg_match("/< (200|503)|> GET \//", $formatted['messageOnly'])) {
-          return null;
+          return "\r";
         }
        
         return $formatted;
